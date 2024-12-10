027c0e1ee11acdbf30f54f474d636a7353037851
lalaine-ellie <lalainesiy@gmail.com>
2024-12-10 21:08:41 +0000

    dice rolls

--- dice-roller.js
+++ dice-roller.js
@@ -1,1 +1,13 @@
+// get number of dice rolls example node dice-roller.js 3 (rolls 3 times)
+const numDice = process.argv[2];
 
+const rollDie = () => Math.floor(Math.random() * 6) + 1;
+
+// rolling dice
+let rolls = [];
+for (let i = 0; i < numDice; i++) {
+  rolls.push(rollDie());
+}
+
+// OUTPUT
+console.log(`Rolled ${numDice} dice: ${rolls.join(', ')}`);
